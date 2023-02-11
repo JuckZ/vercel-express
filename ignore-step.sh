495a215f7a79e098a8465d948d4e1db0eff09a81
juck <juckz@foxmail.com>
2023-02-11 09:28:40 +0800

    chore: 1. add ignore-step.sh

---
+++ ignore-step.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+VERCEL_GIT_COMMIT_MESSAGE
+
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
+
+if [[ "$VERCEL_GIT_COMMIT_MESSAGE" =~ "skip ci" || "$VERCEL_GIT_COMMIT_MESSAGE" =~ "skip build"  ]] ; then
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+
+else
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+fi
